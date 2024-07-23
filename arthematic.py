7e40d94de460c393f06ab80bcf18def9607e5f85
M-Deepika-485 <deepadeepikareddy297@gmail.com>
2024-07-23 21:35:41 +0530

    Update arthematic.py

--- arthematic.py
+++ arthematic.py
@@ -1,21 +1,21 @@
-#a=int(input())
-#b=int(input())
-#print(f"add of {a+b} and sub of {a-b} mul of {a*b} div {a/b} power {a^b}")
-#age=int(input())
-#if(age>=18 and age<24):
-#    print("only two wheeler")
-#elif(age>24 and age<45):
-#    print("four wheeler")
-#else:
-#    print("six wheeler")
-# apple=150
-# banana=96
-# orange=40
-# amount=700
-#if ("total cost is <700"):
-#    print("shop keeper was good")
-#else:
-#    print("shop keeper was bad")
+a=int(input())
+b=int(input())
+print(f"add of {a+b} and sub of {a-b} mul of {a*b} div {a/b} power {a^b}")
+age=int(input())
+if(age>=18 and age<24):
+    print("only two wheeler")
+elif(age>24 and age<45):
+    print("four wheeler")
+else:
+    print("six wheeler")
+ apple=150
+ banana=96
+ orange=40
+ amount=700
+if ("total cost is <700"):
+    print("shop keeper was good")
+else:
+    print("shop keeper was bad")
 cost_apple=15
 cost_banana=4
 cost_orange=5
@@ -27,26 +27,26 @@ if("total cost is <700"):
     print("shop keeper was good")
 else:
     print("shop keeper was bad")
-#a=int(input())
-#if(a%2==0 and a<0):
-#   print("given a is  negative even")
-#elif(a%2==0 and a>0):
-#    print("given a is positive even")
-#elif(a%2!=0 and a<0):
-#    print("given a is negative odd")
-#else:
-#     print("given a is positive odd")
-#b_height=int(input("enter the height of x"))
-#b_weight=int(input("enter the weight of x"))
-#b_bodyfat=int(input("enter the body fat of x"))
-#t_height=int(input("enter the height of t"))
-#t_weight=int(input("enter the weight of t"))
-#t_bodyfat=int(input("enter the bodyfat of t"))
-#medals_of_z=(50*14)/100
-#if("b_height==140 and b_weight%==0 and b_b0dyfatt<12)and((t_height<148) and t_weight%medals_of_z==0 and t_bodyfat==140"):
-#    print("all three players are travelling in the same plane from india")
-#else:
-#    print("all three players atre not travelling in the same plane")
+a=int(input())
+if(a%2==0 and a<0):
+   print("given a is  negative even")
+elif(a%2==0 and a>0):
+    print("given a is positive even")
+elif(a%2!=0 and a<0):
+    print("given a is negative odd")
+else:
+     print("given a is positive odd")
+b_height=int(input("enter the height of x"))
+b_weight=int(input("enter the weight of x"))
+b_bodyfat=int(input("enter the body fat of x"))
+t_height=int(input("enter the height of t"))
+t_weight=int(input("enter the weight of t"))
+t_bodyfat=int(input("enter the bodyfat of t"))
+medals_of_z=(50*14)/100
+if("b_height==140 and b_weight%==0 and b_b0dyfatt<12)and((t_height<148) and t_weight%medals_of_z==0 and t_bodyfat==140"):
+    print("all three players are travelling in the same plane from india")
+else:
+    print("all three players atre not travelling in the same plane")
 
 
 

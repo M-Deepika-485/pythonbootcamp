d17aa8faa4402b625a6fcba9f012cc7b8f02567e
M-Deepika-485 <deepadeepikareddy297@gmail.com>
2024-07-23 21:41:09 +0530

    Update ascii_2.py

--- ascii_2.py
+++ ascii_2.py
@@ -1,19 +1,19 @@
 #remove all the brackets from the given algebric expression
-#str=input()
-#for i in str:
-#    if(ord(i)==40 or ord(i)==41 or ord(i)==91 or ord(i)==93 or ord(i)==123 or ord(i)==125):
-#        pass
-#    else:
-#       print(i,end="")
-#print()
+str=input()
+for i in str:
+    if(ord(i)==40 or ord(i)==41 or ord(i)==91 or ord(i)==93 or ord(i)==123 or ord(i)==125):
+        pass
+    else:
+       print(i,end="")
+print()
 #write a program to print next letters of given
-#inp="ABC"
-#char=''
-#n=0
-#for i in inp:
-#    n=ord(i)
-#    char+=chr(n+4)
-#print(char)
+inp="ABC"
+char=''
+n=0
+for i in inp:
+    n=ord(i)
+    char+=chr(n+4)
+print(char)
 #input=hello-----world then output should be -----helloworld
 inp="hello-----wor---ld"
 inp1=''

e5c2371e5edfb588ff3466ec0e44fb0e34546b0a
M-Deepika-485 <deepadeepikareddy297@gmail.com>
2024-07-23 21:44:58 +0530

    Update problem9_1.py

--- problem9_1.py
+++ problem9_1.py
@@ -1,27 +1,27 @@
-#n=123
-#sum=0
-#while n>0:
-#    r=n%10
-#    sum=sum+r
-#    n=n//10
-#print(sum)    
+n=123
+sum=0
+while n>0:
+    r=n%10
+    sum=sum+r
+    n=n//10
+print(sum)    
 #find the sum of square of a digit in a given number
-#n=123
-#sum=0
-#while n>0:
-#    r=n%10
-#    sum=sum+r**2
-#    n=n//10
-#print(sum)    
+n=123
+sum=0
+while n>0:
+    r=n%10
+    sum=sum+r**2
+   n=n//10
+print(sum)    
 #sum of even digits
-#n=1234
-#sum=0
-#while n>0:
-#    r=n%10
-#    if(r%2==0):
-#        sum+=r
-#    n=n//10
-#print(sum)    
+n=1234
+sum=0
+while n>0:
+    r=n%10
+    if(r%2==0):
+        sum+=r
+    n=n//10
+print(sum)    
 #reverse number
 n=123
 rev=""
